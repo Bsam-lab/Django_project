8aaa640c4784e1f78110e1d2817189ca82b7ff1b
bsam <samsonokunlola1@gmail.com>
2025-03-06 19:59:45 +0100

    Update

--- main_app/models.py
+++ main_app/models.py
@@ -17,12 +17,12 @@ class Downbase(models.Model):
     def __str__(self):
         return self.social_handle
     
-class Veggie(models.Model):
-    company_name = models.CharField(max_length=50)
-    todo = models.CharField(max_length=100)
-    percent = models.CharField(max_length=50)
-    def __str__(self):
-        return self.company_name
+# class Veggie(models.Model):
+#     company_name = models.CharField(max_length=50)
+#     todo = models.CharField(max_length=100)
+#     percent = models.CharField(max_length=50)
+#     def __str__(self):
+#         return self.company_name
     
 class Logistic(models.Model):
     icon= models.CharField(max_length=100)
